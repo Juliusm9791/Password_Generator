e012c008960833f46552e6f6f8460f27d165a5a0
Julius Markauskas <julusm9791@gmail.com>
2021-09-04 14:09:41 -0400

    lots of updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,54 @@
 let charArray = ["qwertyuiopasdfghjklzxcvbnm", //0
                  "QWERTYUIOPASDFGHJKLZXCVBNM", //1
                  "1234567890",                 //2
-                 "!@#$%&-"]                    //3
+                 "-=~!@#$%^&*()_+[]{}|?"]   //3
 
-let paswLength = 8;
-let capsInPasw = 1;
-let numbInPasw = 1;
-let charInPasw = 1;
+let paswLength;
+let howCapsInPasw;
+let howNumbInPasw;
+let howSymbInPasw;
 
-// paswLength = prompt("Choose password length");
-// capsInPasw = prompt("How many capsletters in password");
-// numbInPasw = prompt("How many numbers in password");
-// charInPasw = prompt("How many characters in password");
+function readValues() {
+    paswLength = document.getElementById("forPaswLength").value;
+    // console.log(paswLength);
+    howCapsInPasw = document.getElementById("forPaswCap").value;
+    howNumbInPasw = document.getElementById("forPaswNum").value;
+    howSymbInPasw = document.getElementById("forPaswSym").value;
+}
 
-console.log(paswLength);
+document.getElementById("advance").addEventListener("change", updateText);
+
+document.getElementById("showAdvance").addEventListener("change", updateVisibility);
+
+function updateVisibility() {
+    if (this.checked) {
+        document.getElementById("advance").style.display = "block";
+    } else {
+        document.getElementById("advance").style.display = "none";
+    }
+
+}
+
+function updateText() {
+    readValues();
+    document.getElementById("changePaswLabel").textContent = "Password Length (8-128): " + paswLength;
+    document.getElementById("changeCapLabel").textContent = "Cap Lettes in Password: " + howCapsInPasw;
+    document.getElementById("changeNumLabel").textContent = "Numbers in Password: " + howNumbInPasw;
+    document.getElementById("changeSymLabel").textContent = "Symbols in Password: " + howSymbInPasw;
+}
+
+
+//           Password Generator
 
 function pickRandom(x,y) {
     let pasw = "";
-    // console.log(x);
      for (i = 0; i < x; i++) {
-        let random = Math.floor((Math.random() * charArray[y].length));
-        let charFromArray = charArray[y].charAt(random);
-        // console.log(charFromArray)
+        let randomPlace = Math.floor((Math.random() * charArray[y].length));
+        let charFromArray = charArray[y].charAt(randomPlace);
         pasw += charFromArray;        
     }
     return pasw;
 }
-
-let lowerLetters = pickRandom((paswLength = paswLength - numbInPasw - capsInPasw - charInPasw),0);
-let capInPasw = pickRandom(capsInPasw,1);
-let numbersInPasw = pickRandom(numbInPasw,2);
-let symbolInPasw = pickRandom(charInPasw,3);
-
-let newPassword = lowerLetters + capInPasw + numbersInPasw + symbolInPasw;
-console.log(newPassword);
 
 function shuffle(){
     let z = newPassword.split("");
@@ -46,7 +61,18 @@ function shuffle(){
 return z.join("");
 }
 
-let shuffledPasw = shuffle();
-console.log(shuffledPasw);
+let newPassword = "Your Secure Password";
 
-
+function genAndShow() {
+    readValues();
+    let lowerLetters = pickRandom((paswLength = paswLength - howNumbInPasw - howCapsInPasw - howSymbInPasw),0);
+    let capInPasw = pickRandom(howCapsInPasw,1);
+    let numbersInPasw = pickRandom(howNumbInPasw,2);
+    let symbolInPasw = pickRandom(howSymbInPasw,3);
+    newPassword = lowerLetters + capInPasw + numbersInPasw + symbolInPasw;
+    console.log(newPassword);
+    let shuffledPasw = shuffle();
+    document.getElementById("showPasw").innerHTML = shuffledPasw;
+    console.log(shuffledPasw);
+    document.getElementById("len").innerHTML = shuffledPasw.length;
+}
